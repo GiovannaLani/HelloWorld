275042bf0afd758458077ee5f16e22f4fddc3b1c
GiovannaLani <giovanna.lani@opendeusto.es>
2024-02-29 09:38:15 +0100

    color

--- Assets/Transform2.cs
+++ Assets/Transform2.cs
@@ -7,6 +7,7 @@ public class Transform2 : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] //Para verlo en unity
     private GameObject cube;
+  
     void Start()
     {
         //cube = GameObject.Find("Cube");
